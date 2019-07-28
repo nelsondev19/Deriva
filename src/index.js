361c7200a4c761e8fa2c7e3485b8c101d001617a
IsseiMendez <51303481+IsseiMendez@users.noreply.github.com>
2019-07-27 18:44:13 -0600

    :v

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const uuid = require('uuid/v4');
-const fs = require('fs-extra')
 const ctrl = {}
 
 const storage = multer.diskStorage({
@@ -32,7 +31,6 @@ app.set('view engine', 'ejs');
 // middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-/*
 app.use(multer({
   storage,
   dest: path.join(__dirname, 'public/img'),
@@ -48,7 +46,7 @@ app.use(multer({
   }
 }).single('img'));
 
-
+/*
 ctrl.create = async (req, res) => {
   const imgUrl = randomNumber();
   console.log(imgUrl);
@@ -65,41 +63,6 @@ ctrl.create = async (req, res) => {
   console.log(newimg)
 }
 */
-
-ctrl.create = (req, res) => {
-  const saveImage = async () => {
-    const imgUrl = randomNumber();
-    const images = await Image.find({ filename: imgUrl });
-    if (images.length > 0) {
-      saveImage()
-    } else {
-      // Image Location
-      const imageTempPath = req.file.path;
-      const ext = path.extname(req.file.originalname).toLowerCase();
-      const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
-
-      // Validate Extension
-      if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-        // you wil need the public/temp path or this will throw an error
-        await fs.rename(imageTempPath, targetPath);
-        const newImg = new Image({
-          title: req.body.title,
-          filename: imgUrl + ext,
-          description: req.body.description
-        });
-        const imageSaved = await newImg.save();
-        res.redirect('/images/' + imageSaved.uniqueId);
-      } else {
-        await fs.unlink(imageTempPath);
-        res.status(500).json({ error: 'Only Images are allowed' });
-      }
-    }
-  };
-
-  saveImage();
-};
-
-
 // routes
 const indexRoutes = require('./routes/index');
 app.use(indexRoutes);
